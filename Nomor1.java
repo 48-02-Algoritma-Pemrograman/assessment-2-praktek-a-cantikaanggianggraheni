516dacfe8c6941d47f085c49ae8038f2451332a2
cantikaanggianggraheni <cantikaanggianggraheni@gmail.com>
2024-12-12 12:12:36 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,6 +6,23 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        scanner.close();
+        printFizzBuzz(n);
+    }
+
+    public static void printFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " tidak bisa");
+            }
+        }
+    
 
         /* Pekerjaan anda berakhir sini */
 
